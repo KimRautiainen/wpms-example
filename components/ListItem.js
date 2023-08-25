2248894cfd7499159f3add5654f46143bf8e5510
KimRautiainen <kim.rautiainen@hotmail.com>
2023-08-25 09:45:43 +0300

    asd

---
+++ components/ListItem.js
@@ -0,0 +1,23 @@
+import { FlatList, Image, Text, TouchableOpacity } from "react-native";
+import PropTypes from 'prop-types';
+
+const ListItem = (props) => {
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                console.log('touched!', props.singleMedia.title);
+            }}
+        >
+            <Image
+                style={{ width: 100, height: 100 }}
+                source={{ uri: props.singleMedia.thumbnails.w160 }}
+            />
+            <Text>{props.singleMedia.title}</Text>
+            <Text>{props.singleMedia.description}</Text>
+        </TouchableOpacity>
+    );
+};
+ListItem.prototypes = {
+    singleMedia: PropTypes.object,
+};
+export default ListItem;
