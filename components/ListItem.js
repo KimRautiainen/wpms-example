9290b3c20ea37f7c2f07dea63299eda6df920b53
KimRautiainen <kim.rautiainen@hotmail.com>
2023-08-26 15:17:56 +0300

    http-b ready

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,23 +2,24 @@ import { FlatList, Image, Text, TouchableOpacity } from "react-native";
 import PropTypes from 'prop-types';
 import { mediaUrl } from "../utils/app-config";
 
-const ListItem = ({singleMedia}) => {
-    return (
-        <TouchableOpacity
-            onPress={() => {
-                console.log('touched!', singleMedia.title);
-            }}
-        >
-            <Image
-                style={{ width: 100, height: 100 }}
-                source={{ uri: mediaUrl + singleMedia.thumbnails.w160 }}
-            />
-            <Text>{singleMedia.title}</Text>
-            <Text>{singleMedia.description}</Text>
-        </TouchableOpacity>
-    );
+
+const ListItem = ({ singleMedia }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        console.log('touched!', singleMedia.title);
+      }}
+    >
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{ uri: mediaUrl + singleMedia.thumbnails.w160 }}
+      />
+      <Text>{singleMedia.title}</Text>
+      <Text>{singleMedia.description}</Text>
+    </TouchableOpacity>
+  );
 };
 ListItem.prototypes = {
-    singleMedia: PropTypes.object,
+  singleMedia: PropTypes.object,
 };
 export default ListItem;
