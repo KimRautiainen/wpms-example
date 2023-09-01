5ad445c906f9cfd977a1beb1195db88f73e7b243
KimRautiainen <kim.rautiainen@hotmail.com>
2023-09-01 11:02:06 +0300

    add token to async storage, dummy-login ready

--- views/Profile.js
+++ views/Profile.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import {Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, { useContext } from 'react';
+import { Platform, SafeAreaView, StyleSheet, Text, Button } from 'react-native';
+import { MainContext } from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = (props) => {
+  const { setIsLoggedIn } = useContext(MainContext);
+  const logOut = async () => {
+    console.log("profile, logout");
+    try {
+      await AsyncStorage.clear();
+      setIsLoggedIn(false);
+    } catch (e) {
+      console.log(error);
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile view</Text>
+      <Button title='LogOut' onPress={logOut} />
     </SafeAreaView>
   );
 };
